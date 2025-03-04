3f94de3f01769b6cfd682f893a28d1b9251f439b
leila <leila.m2006131@gmail.com>
2025-03-04 22:39:18 +0330

    Code Student completion

--- src/uni/Student.java
+++ src/uni/Student.java
@@ -11,12 +11,26 @@ public class Student {
     public String studentID;
 
     public Student(int personID, int enteranceYear, int majorID) {
+        this.id = id;
         this.personID = personID;
         this.enteranceYear = enteranceYear;
         this.majorID = majorID;
     }
     public static Student findById(int id) {
+        for(Student student : studentList) {
+            if(student.id == id) {
+                return student;
+            }
+        }
         return null;
     }
-    public void setStudentCode() {}
+    public void setStudentCode() {
+        int count = 0;
+        for(Student student : studentList) {
+            if(student.majorID == this.majorID){
+                count++;
+            }
+        }
+        this.studentID = String.format("%d%02d%03d", this.enteranceYear, this.majorID, count + 1);
+    }
 }
