cd1ca37ec161588e34bf215c284d9f3694776178
leila <leila.m2006131@gmail.com>
2025-03-04 22:59:50 +0330

    Code PeresentedCourse completion

--- src/uni/PeresentedCourse.java
+++ src/uni/PeresentedCourse.java
@@ -10,7 +10,27 @@ public class PeresentedCourse {
     public int capacity;
     public ArrayList<Integer> studentIds = new ArrayList<>();
 
-    public PeresentedCourse(int CourseID, int professorID, int maxCapacity) {}
-    public static PeresentedCourse findById(int id) { return null;}
-    public void addStudent(int studentID) {}
+    public PeresentedCourse(int CourseID, int professorID, int maxCapacity) {
+        this.CourseID = CourseID;
+        this.professorID = professorID;
+        this.capacity = maxCapacity;
+        PeresentedCourse.peresentedCourselist.add(this);
+        this.id = peresentedCourselist.size();
+    }
+    public static PeresentedCourse findById(int id) {
+        for (PeresentedCourse p : peresentedCourselist) {
+            if (p.id == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+    public void addStudent(int studentID) {
+        if(studentID < capacity){
+            studentIds.add(studentID);
+        }
+        else {
+            System.out.println("خطا! ظرفیت به حداکثر تعداد رسیده است.");
+        }
+    }
 }
